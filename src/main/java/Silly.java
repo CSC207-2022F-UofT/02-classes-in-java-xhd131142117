c123943a855a32b9acfe07f10ef3e619505c94d1
Scavolamax <37249237+xhd131142117@users.noreply.github.com>
2022-10-09 21:23:58 -0400

    Attempt 1

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -79,7 +79,14 @@ public class Silly implements Comparable<Silly>{
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
      */
-
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two strings as an argument.
+     *
+     * @param first a string for this Silly instance's name.
+     * @param second a string for this Silly instance's name.
+     */
+    public Silly(String first, String second) {this.name = first + second;};
 
 
 
@@ -116,7 +123,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +141,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -158,6 +166,8 @@ public class Silly implements Comparable<Silly>{
         }
 
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
+        if(this.name.equals(other.name)) {return true;}
+        return false;
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
@@ -194,6 +204,7 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        return this.name.length() - other.name.length();
     }
 
     /*
